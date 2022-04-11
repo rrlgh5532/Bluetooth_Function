78f7d604b685da3c9279917e40887a8f338cf34e
One27 <rrlgh5532@me.com>
2022-04-11 22:54:07 +0900

    .

---
+++ app/src/main/java/com/example/bluetooth_function/CheckConnectionAcitivity.java
@@ -0,0 +1,14 @@
+package com.example.bluetooth_function;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckConnectionAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_connection_acitivity);
+    }
+}
--- app/src/main/java/com/example/bluetooth_function/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_function/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.bluetooth_function;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     public BluetoothAdapter mBtadapter;
+
+    private Button btn_checkConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mBtadapter = BluetoothAdapter.getDefaultAdapter();
+
+        btn_checkConnection = findViewById(R.id.btn_check_Connection);
+        btn_checkConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,CheckConnectionAcitivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        });
     }
 }
