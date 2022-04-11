57b471dab0ea642e4082c3d32aea5e1332eb0db7
One27 <rrlgh5532@gmail.com>
2022-04-11 21:17:43 +0900

    fisrt

--- app/src/main/java/com/example/bluetooth_function/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_function/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.bluetooth_function;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public BluetoothAdapter mBtadapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtadapter = BluetoothAdapter.getDefaultAdapter();
     }
 }
