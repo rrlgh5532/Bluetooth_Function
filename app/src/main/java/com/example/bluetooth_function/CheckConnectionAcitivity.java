c743f97f640a250eb682ef1a3076a37a5c52f0ca
One27 <rrlgh5532@me.com>
2022-04-12 00:15:27 +0900

    add bluetooth permission

--- app/src/main/java/com/example/bluetooth_function/CheckConnectionAcitivity.java
+++ app/src/main/java/com/example/bluetooth_function/CheckConnectionAcitivity.java
@@ -1,14 +1,83 @@
 package com.example.bluetooth_function;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.bluetooth_function.MainActivity.mBtadapter;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class CheckConnectionAcitivity extends AppCompatActivity {
+
+    private ListView listView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_connection_acitivity);
+
+
+        listView = findViewById(R.id.lv_check_connection_paird_items);
+        List<String> list = new ArrayList<>();
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String data = (String) parent.getItemAtPosition(position);
+
+            }
+        });
+
+        if (mBtadapter == null) {
+            // device doesn't support bluetooth
+        } else {
+
+            // bluetooth is off, ask user to on it.
+            if (!mBtadapter.isEnabled()) {
+                Intent enableAdapter = new Intent(mBtadapter.ACTION_REQUEST_ENABLE);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivityForResult(enableAdapter, 0);
+            }
+
+            // Do whatever you want to do with your bluetoothAdapter
+            Set<BluetoothDevice> all_devices = mBtadapter.getBondedDevices();
+            if (all_devices.size() > 0) {
+                for (BluetoothDevice currentDevice : all_devices) {
+                    //log.i("Device Name " + currentDevice.getName());
+                    adapter.add(currentDevice.getName());
+                }
+            }
+            adapter.notifyDataSetChanged();
+        }
     }
 }
--- app/src/main/java/com/example/bluetooth_function/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_function/MainActivity.java
@@ -2,15 +2,17 @@ package com.example.bluetooth_function;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    public BluetoothAdapter mBtadapter;
+    public static BluetoothAdapter mBtadapter;
 
     private Button btn_checkConnection;
 
@@ -30,5 +32,30 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        checkPermission();
+    }
+    private void checkPermission(){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            requestPermissions(
+                    new String[]{
+                            Manifest.permission.BLUETOOTH,
+                            Manifest.permission.BLUETOOTH_SCAN,
+                            Manifest.permission.BLUETOOTH_ADVERTISE,
+                            Manifest.permission.BLUETOOTH_CONNECT
+
+
+                    },
+                    1);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(
+                    new String[]{
+                            Manifest.permission.BLUETOOTH
+
+                    },
+                    1);
+        }
+
     }
 }
